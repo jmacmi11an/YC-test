7a5cded9a27c8f276d550abfd65e7664f4619b2a
Jonathan C MacMillan <jmacmill@gmail.com>
2020-10-04 13:08:41 +1100

    first hour

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,18 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+ const path = require(`path`)
+
+ module.exports = {
+   plugins: [
+     {
+       resolve: `gatsby-source-filesystem`,
+       options: {
+         name: `images`,
+         path: path.join(__dirname, `src`, `images`),
+       },
+     },
+     `gatsby-plugin-sharp`,
+     `gatsby-transformer-sharp`,
+   ],
+ }
